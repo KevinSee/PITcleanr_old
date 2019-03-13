ec23b6fa306bc964a452bce64c5a0038a4557cbc
Kevin See <Kevin@qcinc.org>
2019-03-13 13:23:53 -0700

    Added 2 functions to help determine the path to a node, based on RKM values.

---
+++ R/getUpStrmPath.R
@@ -0,0 +1,28 @@
+#' @title Upstream path
+#'
+#' @description For a given site, uses the configuration file to determine which path (through which sites) must be taken from the \code{rootSite} to reach that site.
+#'
+#' @author Kevin See
+#'
+#' @inheritParams findDwnStrmSite
+#'
+#' @import dplyr stringr
+#' @return NULL
+#' @examples
+
+getUpStrmPath = function(site, configurationFile, include.MMR.sites = F, rootSite = 'GRA') {
+  sitePath = NULL
+  dwnStrmSite = findDwnStrmSite(site,
+                                configurationFile,
+                                include.MMR.sites = include.MMR.sites,
+                                rootSite = rootSite)
+  sitePath = c(site, dwnStrmSite)
+  while(sum(grepl(rootSite, sitePath)) == 0) {
+    dwnStrmSite = findDwnStrmSite(dwnStrmSite,
+                                  configurationFile,
+                                  include.MMR.sites = include.MMR.sites,
+                                  rootSite = rootSite)
+    sitePath = c(sitePath, dwnStrmSite)
+  }
+  return(rev(sitePath))
+}
